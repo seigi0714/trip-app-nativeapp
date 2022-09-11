2c776024b273c83a346dbb571611c08f6bd05b2a
shimizu-saffle <shimizu.saffle@gmail.com>
2022-09-11 21:15:16 +0900

    feat: Widget テスト用のフィールドを TripApp に追加。ファイル名を変更。

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'config/trip_app.dart';
+import 'config/app.dart';
 
 void main() {
   runApp(
-    const ProviderScope(child: TripApp()),
+    const TripApp(),
   );
 }
