a7851c3925ab62e5ac0da31e3d47533cc73cdcc2
shimizu-saffle <shimizu.saffle@gmail.com>
2022-09-16 21:44:20 +0900

    define: LoginPage Route

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:trip_app_nativeapp/pages/login_page.dart';
 
 import '../pages/constant_page.dart';
 
 final routerProvider = Provider<GoRouter>(
   (ref) {
     return GoRouter(
+      // TODO(shimizu-saffle): ページが増え次第、initialLocation を変更して、適切なリダイレクト処理を行う。
+      initialLocation: LoginPage.path,
       routes: [
-        // 以下のようにルートを定義していく
-        // GoRoute(
-        //   path: ExamplePage.path,
-        //   name: ExamplePage.name,
-        //   pageBuilder: (context, state) => MaterialPage(
-        //     key: state.pageKey,
-        //     child: const ExamplePage(),
-        //   ),
-        // ),
+        GoRoute(
+          path: LoginPage.path,
+          name: LoginPage.name,
+          builder: (context, state) => const LoginPage(),
+        ),
       ],
       navigatorBuilder: (context, state, child) {
         return ConstantPage(child: child);
