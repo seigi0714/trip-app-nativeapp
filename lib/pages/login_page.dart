04817f14c5a87019e75af984cc9885f1cacf7408
shimizu-saffle <shimizu.saffle@gmail.com>
2022-09-19 21:39:33 +0900

    feat: LoginPage にアニメーションを表示

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:lottie/lottie.dart';
 import 'package:trip_app_nativeapp/utils/extensions/build_context.dart';
 
 import '../gen/assets.gen.dart';
@@ -16,44 +17,61 @@ class LoginPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            BrandButton(
-              label: 'LINE ログイン',
-              brandLogo: Icon(
-                FontAwesomeIcons.line,
-                size: context.displaySize.width * 0.08,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const Spacer(flex: 2),
+              Text(
+                'さあ、旅に出よう',
+                style: context.textTheme.headline4?.copyWith(
+                  color: kPrimaryColor,
+                ),
               ),
-              backgroundColor: lineGreen,
-              onPressed: () {
-                // TODO(shimizu-saffle): LINEでログイン
-              },
-            ),
-            BrandButton(
-              label: 'Google ログイン',
-              labelColor: Colors.black54,
-              brandLogo: Assets.images.googleLogo.image(
-                height: context.displaySize.width * 0.08,
+              const Spacer(),
+              Lottie.asset(
+                Assets.lotties.travelLoading,
+                height: context.displaySize.height * 0.5,
               ),
-              backgroundColor: Colors.white,
-              onPressed: () {
-                // TODO(shimizu-saffle): LINEでログイン
-              },
-            ),
-            BrandButton(
-              label: 'Apple ログイン',
-              brandLogo: Icon(
-                FontAwesomeIcons.apple,
-                size: context.displaySize.width * 0.08,
+              const Spacer(),
+              BrandButton(
+                label: 'LINE ログイン',
+                brandLogo: Icon(
+                  FontAwesomeIcons.line,
+                  size: context.displaySize.width * 0.08,
+                ),
+                backgroundColor: lineGreen,
+                onPressed: () {
+                  // TODO(shimizu-saffle): LINEでログイン
+                },
               ),
-              backgroundColor: Colors.black,
-              onPressed: () {
-                // TODO(shimizu-saffle): LINEでログイン
-              },
-            ),
-          ],
+              const Spacer(),
+              BrandButton(
+                label: 'Google ログイン',
+                labelColor: Colors.black54,
+                brandLogo: Assets.images.googleLogo.image(
+                  height: context.displaySize.width * 0.08,
+                ),
+                backgroundColor: Colors.white,
+                onPressed: () {
+                  // TODO(shimizu-saffle): LINEでログイン
+                },
+              ),
+              const Spacer(),
+              BrandButton(
+                label: 'Apple ログイン',
+                brandLogo: Icon(
+                  FontAwesomeIcons.apple,
+                  size: context.displaySize.width * 0.08,
+                ),
+                backgroundColor: Colors.black,
+                onPressed: () {
+                  // TODO(shimizu-saffle): LINEでログイン
+                },
+              ),
+              const Spacer(flex: 2),
+            ],
+          ),
         ),
       ),
     );
