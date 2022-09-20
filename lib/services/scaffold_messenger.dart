e484966374bbd0031a913933e8562f8db30831b0
shimizu-saffle <shimizu.saffle@gmail.com>
2022-09-20 22:22:59 +0900

    refactor: ファイル名修正

--- lib/services/scaffold_messenger.dart
+++ lib/services/scaffold_messenger.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../../utils/extensions/string.dart';
-import '../../utils/constants/string.dart';
+import '../../utils/extensions/string.dart';
+import '../utils/constants/string.dart';
 
 final scaffoldMessengerKeyProvider =
     Provider((_) => GlobalKey<ScaffoldMessengerState>());
