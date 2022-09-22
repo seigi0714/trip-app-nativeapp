cff2fd718ddce38a337d2eebe0dbb1f230717d41
shimizu-saffle <shimizu.saffle@gmail.com>
2022-09-22 22:48:33 +0900

    define: headerInterceptorProvider HeaderInterceptor を提供する Provider

--- lib/services/rest_api/dio/header_interceptor.dart
+++ lib/services/rest_api/dio/header_interceptor.dart
@@ -1,12 +1,17 @@
 import 'package:dio/dio.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../id_token.dart';
 
+final headerInterceptorProvider =
+    Provider<HeaderInterceptor>(HeaderInterceptor.new);
+
 /// ヘッダーに認証情報などを付加する
 class HeaderInterceptor extends Interceptor {
-  HeaderInterceptor([this.overwriteUrl]);
+  HeaderInterceptor(this._ref, [this.overwriteUrl]);
 
   String? overwriteUrl;
+  final ProviderRef<HeaderInterceptor> _ref;
 
   @override
   Future<void> onRequest(
@@ -16,9 +21,9 @@ class HeaderInterceptor extends Interceptor {
     options.headers['Referer'] = overwriteUrl ?? options.baseUrl;
     options.headers['Origin'] = options.baseUrl;
     options.headers['Accept'] = 'application/json';
-    final token = await idToken;
-    if (token.isNotEmpty) {
-      options.headers['Authorization'] = 'IdToken $token';
+    final idToken = _ref.watch(idTokenProvider);
+    if (idToken.isNotEmpty) {
+      options.headers['Authorization'] = 'IdToken $idToken';
     }
     return handler.next(options);
   }
