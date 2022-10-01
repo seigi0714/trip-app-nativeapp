587d25b25f08a06cbbc60fa60ed07091cffed2d0
shimizu-saffle <shimizu.saffle@gmail.com>
2022-10-01 11:23:38 +0900

    feat: LineSDK setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_line_sdk/flutter_line_sdk.dart';
 
 import 'config/app.dart';
+import 'env/env.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await LineSDK.instance.setup(Env.lineChannelId);
   runApp(
     const TripApp(),
   );
