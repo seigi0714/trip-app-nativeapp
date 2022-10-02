32f81ccea191de45cec8431bf00a3484829debdc
shimizu-saffle <shimizu.saffle@gmail.com>
2022-10-02 17:12:51 +0900

    chore: .env => .env.local

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -2,7 +2,7 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-@Envied(path: '.env')
+@Envied(path: '.env.local')
 abstract class Env {
   @EnviedField(
     varName: 'LINE_CHANNEL_ID',
