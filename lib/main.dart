8290318969c673d0920cab3555b5d0b376a0cbd3
shimizu-saffle <shimizu.saffle@gmail.com>
2022-10-02 18:39:29 +0900

    chore: flutterfire configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_line_sdk/flutter_line_sdk.dart';
 
 import 'config/app.dart';
 import 'env/env.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await LineSDK.instance.setup(Env.lineChannelId);
   runApp(
     const TripApp(),
