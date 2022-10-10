f4a4b4e8510380441486dd488ad60042f17c9347
shimizu-saffle <shimizu.saffle@gmail.com>
2022-10-10 17:41:48 +0900

    feat: LoginPage の実装に伴って、アプリの起動テストの内容を変更

--- test/config/app_test.dart
+++ test/config/app_test.dart
@@ -1,12 +1,20 @@
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:trip_app_nativeapp/config/app.dart';
-import 'package:trip_app_nativeapp/pages/constant_page.dart';
+import 'package:trip_app_nativeapp/pages/login_page.dart';
+import 'package:trip_app_nativeapp/repositories/auth_repository.dart';
 
 void main() {
-  testWidgets('アプリの起動テスト, ConstantPage が存在しているはず。', (tester) async {
+  testWidgets('アプリの初回起動時は、LoginPage が表示されるはず。', (tester) async {
     await tester.pumpWidget(
-      const TripApp(),
+      TripApp(
+        overrides: [
+          firebaseAuthProvider.overrideWithValue(
+            MockFirebaseAuth(),
+          ),
+        ],
+      ),
     );
-    expect(find.byType(ConstantPage), findsOneWidget);
+    expect(find.byType(LoginPage), findsOneWidget);
   });
 }
