8bd118c196514d33e49b391fb21abe8751d02b9a
shimizu-saffle <shimizu.saffle@gmail.com>
2022-10-14 08:42:03 +0900

    define: AuthLogic が担うべきハンドリングをリポジトリから削除

--- lib/repositories/auth/line_login_repository.dart
+++ lib/repositories/auth/line_login_repository.dart
@@ -2,8 +2,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_line_sdk/flutter_line_sdk.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../models/api/exception/app_exception.dart';
-
 final lineLoginRepositoryProvider = Provider<LineLoginRepository>(
   (_) => LineLoginRepository(),
 );
@@ -14,15 +12,8 @@ class LineLoginRepository {
       final result = await LineSDK.instance.login(
         scopes: ['profile', 'openid', 'email'],
       );
-      final idToken = result.accessToken.idTokenRaw;
-      final nonce = result.idTokenNonce;
-      if (idToken == null || nonce == null) {
-        throw const AppException(message: 'idToken または nonce が取得できませんでした');
-      }
       return result;
     } on PlatformException {
-      rethrow;
-    } on AppException {
       rethrow;
     } on Exception {
       rethrow;
