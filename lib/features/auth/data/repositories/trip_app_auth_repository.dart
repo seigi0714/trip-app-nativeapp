b6930a388dabe4946b4e8f99aabae1a79e3fef3f
shimizu-saffle <shimizu.saffle@gmail.com>
2022-10-15 15:50:56 +0900

    refactor: loginWithIdToken()

--- lib/features/auth/data/repositories/trip_app_auth_repository.dart
+++ lib/features/auth/data/repositories/trip_app_auth_repository.dart
@@ -1,7 +1,5 @@
-import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../../../core/exception/api_exception.dart';
 import '../../../../core/http/api_client/api_client.dart';
 import '../../domain/entity/custom_token.dart';
 import '../../domain/repositories/trip_app_auth_interface.dart';
@@ -23,21 +21,13 @@ class TripAppAuthRepository implements TripAppAuthInterface {
     required String idToken,
     required String nonce,
   }) async {
-    try {
-      final response = await _apiClient.post(
-        '/api/v1/auth/signup/line',
-        data: <String, String>{
-          'line_id_token': idToken,
-          'nonce': nonce,
-        },
-      );
-      return PostLoginResponse.fromJson(response.data).customToken;
-    } on DioError {
-      rethrow;
-    } on ApiException {
-      rethrow;
-    } on Exception {
-      rethrow;
-    }
+    final response = await _apiClient.post(
+      '/api/v1/auth/signup/line',
+      data: <String, String>{
+        'line_id_token': idToken,
+        'nonce': nonce,
+      },
+    );
+    return PostLoginResponse.fromJson(response.data).customToken;
   }
 }
