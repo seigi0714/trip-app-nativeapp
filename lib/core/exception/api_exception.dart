21c7deb5415f85600696ed86317d543e31453c8c
shimizu-saffle <shimizu.saffle@gmail.com>
2022-10-15 16:24:31 +0900

    define: ExceptionHandler

--- lib/core/exception/api_exception.dart
+++ lib/core/exception/api_exception.dart
@@ -1,13 +1,13 @@
 /// Trip App API との HTTP 通信時に使用する例外型。
 class ApiException implements Exception {
   const ApiException({
-    this.statusCode,
+    required this.statusCode,
     this.errorCode,
     this.description = 'サーバとの通信に失敗しました。',
   });
 
   /// HTTP ステータスコード
-  final int? statusCode;
+  final int statusCode;
 
   /// Trip App API 独自のエラーコード
   final String? errorCode;
