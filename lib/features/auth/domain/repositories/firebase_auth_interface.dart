7d0f96ce93b7c53cf777d3a659ed280cbd958429
seiginakamura0714 <nakamura_seigi@cyberagent.co.jp>
2022-10-16 15:08:25 +0900

    docs: オブジェクトに関する説明は///

--- lib/features/auth/domain/repositories/firebase_auth_interface.dart
+++ lib/features/auth/domain/repositories/firebase_auth_interface.dart
@@ -1,12 +1,12 @@
 import '../entity/third_perty_credential/third_perty_credential.dart';
 
 abstract class FirebaseAuthInterface {
-  // Firebase customtokenを用いたログイン
+  /// Firebase customtokenを用いたログイン
   Future<void> signInWithCustomToken({
     required String customToken,
   });
-  //　Oauthログインで生成されたCredentialを用いたログイン
+  ///　Oauthログインで生成されたCredentialを用いたログイン
   Future<void> signInWithGoogle(ThirdPertyCredential credential);
-  // ログアウト
+  /// ログアウト
   Future<void> signOut();
 }
