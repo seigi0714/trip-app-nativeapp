3d50343008ae633b2d4281639153fb3ba15a5f06
shimizu-saffle <shimizu.saffle@gmail.com>
2022-10-16 11:32:33 +0900

    refactor: signInWithCustomToken() の返り値を void に reason: 現状、UserCredential を使ってないため

--- lib/features/auth/domain/repositories/firebase_auth_interface.dart
+++ lib/features/auth/domain/repositories/firebase_auth_interface.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
 abstract class FirebaseAuthInterface {
-  Future<UserCredential> signInWithCustomToken({
+  Future<void> signInWithCustomToken({
     required String customToken,
   });
 
