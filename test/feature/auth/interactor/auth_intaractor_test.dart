9b97da69185240631828da756640877cc7bbc3b2
seiginakamura0714 <nakamura_seigi@cyberagent.co.jp>
2022-10-17 21:29:52 +0900

    test: 純正常系のテストを実装

--- test/feature/auth/interactor/auth_intaractor_test.dart
+++ test/feature/auth/interactor/auth_intaractor_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:trip_app_nativeapp/core/exception/app_exception.dart';
 import 'package:trip_app_nativeapp/features/auth/data/repositories/firebase_auth_repository.dart';
 import 'package:trip_app_nativeapp/features/auth/data/repositories/google_login_repository.dart';
 import 'package:trip_app_nativeapp/features/auth/data/repositories/line_login_repository.dart';
@@ -40,6 +41,35 @@ Future<void> main() async {
     accessToken: 'accessToken',
   );
 
+  /*
+   * exception
+   */
+  const unAuthorizationErrorCode = 'UnAuthorization';
+  // LINEログインエラー
+  const lineLoginErrorMessage = 'LINEログインに失敗';
+  const lineLoginException = AppException(
+    code: unAuthorizationErrorCode,
+    message: lineLoginErrorMessage,
+  );
+  // trip_app_apiエラー
+  const tripAppLoginErrorMessage = 'API側でエラー';
+  const tripAppLoginException = AppException(
+    code: unAuthorizationErrorCode,
+    message: tripAppLoginErrorMessage,
+  );
+  // Firebaseログインエラー
+  const firebaseLoginErrorMessage = 'firebaseログインに失敗';
+  const firebaseLoginException = AppException(
+    code: unAuthorizationErrorCode,
+    message: firebaseLoginErrorMessage,
+  );
+  // Googleログインエラー
+  const googleLoginErrorMessage = 'Googleログインに失敗';
+  const googleLoginException = AppException(
+    code: unAuthorizationErrorCode,
+    message: googleLoginErrorMessage,
+  );
+
   setUp(() {
     providerContainer = ProviderContainer(
       overrides: [
@@ -58,6 +88,11 @@ Future<void> main() async {
       ],
     );
     authInteractor = providerContainer.read(authInteractorProvider);
+
+    reset(mockLineLoginRepository);
+    reset(mockFirebaseAuthRepository);
+    reset(mockTripAppRepository);
+    reset(mockGoogleLoginRepository);
   });
   // LINEログイン
   group('AuthIntaractor loginWithLINE', () {
@@ -87,7 +122,143 @@ Future<void> main() async {
         ).called(1);
       },
     );
+
+    test('準正常系 LINEログインに失敗した際は後続の処理が呼ばれず例外を返す', () async {
+      when(mockLineLoginRepository.login()).thenThrow(lineLoginException);
+      
+      await expectLater(
+        () async {
+          await authInteractor.loginWithLINE();
+        },
+        throwsA(
+          isA<AppException>()
+            ..having(
+              (e) => e.code,
+              'errorCode',
+              unAuthorizationErrorCode,
+            ).having(
+              (e) => e.message,
+              'errorMessage',
+              lineLoginErrorMessage,
+            ),
+        ),
+      );
+
+      verifyNever(
+        mockTripAppRepository.loginWithIdToken(
+          idToken: anyNamed('idToken'),
+          nonce: anyNamed('nonce'),
+        ),
+      );
+      verifyNever(
+        mockFirebaseAuthRepository.signInWithCustomToken(
+          customToken: anyNamed('customToken'),
+        ),
+      );
+    });
+
+    test('準正常系 API通信に失敗した場合、Firebaseログインは呼ばれず例外を返す', () async {
+      when(
+        mockLineLoginRepository.login(),
+      ).thenAnswer(
+        (_) async => testOidcInfo,
+      );
+
+      when(
+        mockTripAppRepository.loginWithIdToken(
+          idToken: testOidcInfo.idToken,
+          nonce: testOidcInfo.nonce,
+        ),
+      ).thenThrow(tripAppLoginException);
+
+      await expectLater(
+        () async {
+          await authInteractor.loginWithLINE();
+        },
+        throwsA(
+          isA<AppException>()
+            ..having(
+              (e) => e.code,
+              'errorCode',
+              unAuthorizationErrorCode,
+            ).having(
+              (e) => e.message,
+              'errorMessage',
+              tripAppLoginErrorMessage,
+            ),
+        ),
+      );
+
+      verify(mockLineLoginRepository.login()).called(1);
+      verify(
+        mockTripAppRepository.loginWithIdToken(
+          idToken: testOidcInfo.idToken,
+          nonce: testOidcInfo.nonce,
+        ),
+      ).called(1);
+
+      verifyNever(
+        mockFirebaseAuthRepository.signInWithCustomToken(
+          customToken: anyNamed('customToken'),
+        ),
+      );
+    });
+
+    test('準正常系 Firebaseでのカスタムトークンログインが失敗した場合例外を返す', () async {
+      when(
+        mockLineLoginRepository.login(),
+      ).thenAnswer(
+        (_) async => testOidcInfo,
+      );
+      when(
+        mockTripAppRepository.loginWithIdToken(
+          idToken: testOidcInfo.idToken,
+          nonce: testOidcInfo.nonce,
+        ),
+      ).thenAnswer(
+        (_) async => testCustomToken,
+      );
+
+      when(
+        mockFirebaseAuthRepository.signInWithCustomToken(
+          customToken: testCustomToken,
+        ),
+      ).thenThrow(firebaseLoginException);
+
+      await expectLater(
+        () async {
+          await authInteractor.loginWithLINE();
+        },
+        throwsA(
+          isA<AppException>()
+            ..having(
+              (e) => e.code,
+              'errorCode',
+              unAuthorizationErrorCode,
+            ).having(
+              (e) => e.message,
+              'errorMessage',
+              firebaseLoginErrorMessage,
+            ),
+        ),
+      );
+
+      verify(mockLineLoginRepository.login()).called(1);
+      verify(
+        mockTripAppRepository.loginWithIdToken(
+          idToken: testOidcInfo.idToken,
+          nonce: testOidcInfo.nonce,
+        ),
+      ).called(1);
+
+      verify(
+        mockFirebaseAuthRepository.signInWithCustomToken(
+          customToken: testCustomToken,
+        ),
+      );
+    });
   });
+
   // Googleログイン
   group('AuthIntaractor loginWithGoogle', () {
     test(
@@ -102,5 +273,67 @@ Future<void> main() async {
         ).called(1);
       },
     );
+
+    test('準正常系 googleログインに失敗すると後続の処理が呼ばれず例外を返す', () async {
+      when(mockGoogleLoginRepository.login()).thenThrow(googleLoginException);
+
+      await expectLater(
+        () async {
+          await authInteractor.loginWithGoogle();
+        },
+        throwsA(
+          isA<AppException>()
+            ..having(
+              (e) => e.code,
+              'errorCode',
+              unAuthorizationErrorCode,
+            ).having(
+              (e) => e.message,
+              'errorMessage',
+              googleLoginErrorMessage,
+            ),
+        ),
+      );
+
+      verify(mockGoogleLoginRepository.login()).called(1);
+      verifyNever(mockFirebaseAuthRepository.signInWithGoogle(any));
+    });
+
+    test('準正常系 FirebaseGoogleログインに失敗すると例外を返す', () async {
+      when(
+        mockGoogleLoginRepository.login(),
+      ).thenAnswer(
+        (_) async => testThirdPertyCredential,
+      );
+
+      when(
+        mockFirebaseAuthRepository.signInWithGoogle(testThirdPertyCredential),
+      ).thenThrow(
+        firebaseLoginException,
+      );
+
+      await expectLater(
+        () async {
+          await authInteractor.loginWithGoogle();
+        },
+        throwsA(
+          isA<AppException>()
+            ..having(
+              (e) => e.code,
+              'errorCode',
+              unAuthorizationErrorCode,
+            ).having(
+              (e) => e.message,
+              'errorMessage',
+              firebaseLoginErrorMessage,
+            ),
+        ),
+      );
+
+      verify(mockGoogleLoginRepository.login()).called(1);
+      verify(
+        mockFirebaseAuthRepository.signInWithGoogle(testThirdPertyCredential),
+      ).called(1);
+    });
   });
 }
