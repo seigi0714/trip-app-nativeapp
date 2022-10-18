106caaedf4d182723f0e3bbe265fd23969aec983
seiginakamura0714 <nakamura_seigi@cyberagent.co.jp>
2022-10-18 22:52:31 +0900

    test: 共通で使いそうなクラスのモックを作成

---
+++ test/mock/mock_exception_handler.dart
@@ -0,0 +1,4 @@
+import 'package:mockito/mockito.dart';
+import 'package:trip_app_nativeapp/core/exception/exception_handler.dart';
+
+class MockExceptionHandler extends Mock implements ExceptionHandler {}
---
+++ test/mock/mock_scaffold_messanger_helper.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/mockito.dart';
+import 'package:trip_app_nativeapp/view/widgets/helpers/scaffold_messenger.dart';
+
+class MockScaffoldMessangerHelper extends Mock
+    implements ScaffoldMessengerHelper {}
