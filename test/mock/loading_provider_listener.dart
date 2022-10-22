a901dfb06e1b0cfd2b68ed07dd24cc595979c69e
seigibus <seigibus@icloud.com>
2022-10-22 11:51:45 +0900

    refactor: callメソッドの第２引数名をnextに変更

--- test/mock/loading_provider_listener.dart
+++ test/mock/loading_provider_listener.dart
@@ -3,5 +3,5 @@ import 'package:mockito/mockito.dart';
 /// ローディング関連のプロバイダー内の値を監視するクラス
 class LoadingProviderListener extends Mock {
   // ignore: avoid_positional_boolean_parameters
-  void call(bool? previous, bool value);
+  void call(bool? previous, bool next);
 }
