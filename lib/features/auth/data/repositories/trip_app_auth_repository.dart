777d4d3da2d1b8063c05086fcf971aa29c3dfe2e
seigibus <seigibus@icloud.com>
2022-10-29 12:53:38 +0900

    refactor: privateAPIクライアントをコンストラクタインジェクションするよう修正

--- lib/features/auth/data/repositories/trip_app_auth_repository.dart
+++ lib/features/auth/data/repositories/trip_app_auth_repository.dart
@@ -7,21 +7,26 @@ import '../models/post_login_response/post_login_response.dart';
 
 final tripAppAuthRepositoryProvider = Provider<TripAppAuthInterface>((ref) {
   return TripAppAuthRepository(
-    ref.watch(publicTripAppV1Client),
+    publicV1Client: ref.watch(publicTripAppV1Client),
+    privateV1Client: ref.watch(privateTripAppV1Client),
   );
 });
 
 class TripAppAuthRepository implements TripAppAuthInterface {
-  TripAppAuthRepository(this._apiClient);
+  TripAppAuthRepository({
+    required this.publicV1Client,
+    required this.privateV1Client,
+  });
 
-  final ApiClient _apiClient;
+  final ApiClient publicV1Client;
+  final ApiClient privateV1Client;
 
   @override
   Future<CustomToken> loginWithIdToken({
     required String idToken,
     required String nonce,
   }) async {
-    final response = await _apiClient.post(
+    final response = await publicV1Client.post(
       '/auth/signup/line',
       data: <String, String>{
         'line_id_token': idToken,
@@ -31,9 +36,9 @@ class TripAppAuthRepository implements TripAppAuthInterface {
     return PostLoginResponse.fromJson(response.data).customToken;
   }
 
-  @override 
+  @override
   Future<void> createUserWithFirebaseIdToken({required String name}) async {
-    await _apiClient.post(
+    await privateV1Client.post(
       '/auth/signup/firebase_id_token',
       data: <String, String>{
         'name': name,
