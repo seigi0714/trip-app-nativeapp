b190d744ddaef9bcb38f27de5d98b5949744dcff
seigibus <seigibus@icloud.com>
2022-10-30 12:25:02 +0900

    refactor: AsyncValueのvalueを使ってidTokenを算出するよう修正

--- lib/core/cache/login_token.dart
+++ lib/core/cache/login_token.dart
@@ -3,11 +3,5 @@ import 'package:trip_app_nativeapp/features/auth/controller/auth_controller.dart
 
 final loginTokenProvider = Provider<Future<String>>((ref) {
   final user = ref.watch(firebaseAuthUserProvider);
-  return user.maybeWhen(
-    data: (user) async {
-      final idToken = await user?.getIdToken();
-      return idToken ?? '';
-    },
-    orElse: () async => '',
-  );
+  return user.value?.getIdToken() ?? Future.value('');
 });
