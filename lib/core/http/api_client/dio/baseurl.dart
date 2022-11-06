8982685110e532486dab6f6dd8113c75b353dbbe
shimizu-saffle <shimizu.saffle@gmail.com>
2022-11-06 14:14:22 +0900

    refactor: baseUrlProvider

---
+++ lib/core/http/api_client/dio/baseurl.dart
@@ -0,0 +1,19 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:trip_app_nativeapp/core/http/api_client/api_destination.dart';
+
+import '../../../env.dart';
+
+part 'baseurl.g.dart';
+
+@riverpod
+String baseUrl(
+  BaseUrlRef ref,
+  ApiDestination apiDestination,
+) {
+  switch (apiDestination) {
+    case ApiDestination.publicTripAppV1:
+      return '${Env.protocol}://${Env.tripAppApiUrl}:${Env.tripAppApiPort}/api/v1';
+    case ApiDestination.privateTripAppV1:
+      return '${Env.protocol}://${Env.tripAppApiUrl}:${Env.tripAppApiPort}/private_api/v1';
+  }
+}
