8016198fa83c640b2fc00c33987716da1d541caa
shimizu-saffle <shimizu.saffle@gmail.com>
2022-11-06 15:52:28 +0900

    refactor: googleLoginRepositoryProvider

--- lib/features/auth/data/repositories/google_login_repository.dart
+++ lib/features/auth/data/repositories/google_login_repository.dart
@@ -1,5 +1,5 @@
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../../../core/exception/app_exception.dart';
 import '../../../../core/extensions/google_sign_in_account.dart';
@@ -7,8 +7,12 @@ import '../../../../core/extensions/google_sign_in_authentication.dart';
 import '../../domain/entity/google_account/google_account.dart';
 import '../../domain/repositories/google_login_interface.dart';
 
-final googleLoginRepositoryProvider =
-    Provider<GoogleLoginInterface>((ref) => GoogleLoginRepository());
+part 'google_login_repository.g.dart';
+
+@riverpod
+GoogleLoginInterface googleLoginRepository(GoogleLoginRepositoryRef ref) {
+  return GoogleLoginRepository();
+}
 
 class GoogleLoginRepository implements GoogleLoginInterface {
   @override
@@ -18,7 +22,6 @@ class GoogleLoginRepository implements GoogleLoginInterface {
       throw const AppException(message: 'Googleアカウントの取得に失敗しました。');
     }
     final googleAuth = await googleUser.authentication;
-
 
     if (!googleAuth.isValid) {
       throw const AppException(message: 'idToken または accessToken が取得できませんでした');
