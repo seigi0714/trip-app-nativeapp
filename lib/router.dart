3e26df6e651428b248d834ac0cecffa14ab7fcbc
shimizu-saffle <shimizu.saffle@gmail.com>
2022-11-06 09:34:54 +0900

    refactor: routerProvider

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:trip_app_nativeapp/view/pages/loading_page.dart';
 import 'package:trip_app_nativeapp/view/pages/login_page.dart';
 
@@ -8,46 +8,47 @@ import 'features/auth/controller/auth_controller.dart';
 import 'view/pages/error_page.dart';
 import 'view/pages/home_page.dart';
 
-final routerProvider = Provider<GoRouter>(
-  (ref) {
-    final user = ref.watch(firebaseAuthUserProvider);
-    return GoRouter(
-      redirect: (BuildContext context, state) {
-        final isAtLoginPage = state.subloc == LoginPage.path;
-        if (user.value == null) {
-          return isAtLoginPage ? null : LoginPage.path;
-        } else if (isAtLoginPage) {
-          return HomePage.path;
-        } else {
-          return null;
-        }
-      },
-      routes: [
-        GoRoute(
-          path: HomePage.path,
-          name: HomePage.name,
-          builder: (context, state) => user.when(
-            data: (user) {
-              if (user == null) {
-                return const LoginPage();
-              }
-              return const HomePage();
-            },
-            error: (error, stackTrace) =>
-                ErrorPage(errorMessage: state.error.toString()),
-            loading: () => const LoadingPage(),
-          ),
+part 'router.g.dart';
+
+@riverpod
+GoRouter router(RouterRef ref) {
+  final user = ref.watch(firebaseAuthUserProvider);
+  return GoRouter(
+    redirect: (BuildContext context, state) {
+      final isAtLoginPage = state.subloc == LoginPage.path;
+      if (user.value == null) {
+        return isAtLoginPage ? null : LoginPage.path;
+      } else if (isAtLoginPage) {
+        return HomePage.path;
+      } else {
+        return null;
+      }
+    },
+    routes: [
+      GoRoute(
+        path: HomePage.path,
+        name: HomePage.name,
+        builder: (context, state) => user.when(
+          data: (user) {
+            if (user == null) {
+              return const LoginPage();
+            }
+            return const HomePage();
+          },
+          error: (error, stackTrace) =>
+              ErrorPage(errorMessage: state.error.toString()),
+          loading: () => const LoadingPage(),
         ),
-        GoRoute(
-          path: LoginPage.path,
-          name: LoginPage.name,
-          builder: (context, state) => const LoginPage(),
-        ),
-      ],
-      errorPageBuilder: (context, state) => MaterialPage<Widget>(
-        key: state.pageKey,
-        child: ErrorPage(errorMessage: state.error.toString()),
       ),
-    );
-  },
-);
+      GoRoute(
+        path: LoginPage.path,
+        name: LoginPage.name,
+        builder: (context, state) => const LoginPage(),
+      ),
+    ],
+    errorPageBuilder: (context, state) => MaterialPage<Widget>(
+      key: state.pageKey,
+      child: ErrorPage(errorMessage: state.error.toString()),
+    ),
+  );
+}
