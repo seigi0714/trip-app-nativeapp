b1fe4e8a276258060c0aa7fbda709b6c3e66063d
seigibus <seigibus@icloud.com>
2023-01-15 13:45:40 +0900

    define: tripInteractor定義

---
+++ lib/features/trips/domain/interactor/trip_interactor.dart
@@ -0,0 +1,18 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'trip_interactor.g.dart';
+
+@riverpod
+TripInteractor tripInteractor(TripInteractorRef ref) {
+  return TripInteractor();
+}
+
+class TripInteractor {
+  Future<void> createTrip(
+    String name,
+    DateTime fromDate,
+    DateTime endDate,
+  ) async {
+    // TODO(seigi0714): あとで実装
+  }
+}
