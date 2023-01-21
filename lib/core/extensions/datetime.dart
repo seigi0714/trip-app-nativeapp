dde6e898aa0645c52ba3e0be585a4cd5b7b1ee50
seigibus <seigibus@icloud.com>
2023-01-21 17:14:24 +0900

    feat: jsonで扱うdate型を用意

--- lib/core/extensions/datetime.dart
+++ lib/core/extensions/datetime.dart
@@ -1,18 +1,30 @@
 extension DateTimeExtensions on DateTime {
+  String toJsonDateString() {
+    return '${year.toString()}-${month.toString().padLeft(2, "0")}-${day.toString().padLeft(2, "0")}';
+  }
+
   String toDateString() {
     return '${year.toString()}/${month.toString().padLeft(2, "0")}/${day.toString().padLeft(2, "0")} (${weekdayJpString()})';
   }
 
-  String weekdayJpString() { 
+  String weekdayJpString() {
     switch (weekday) {
-    case DateTime.monday: return '月';
-    case DateTime.tuesday: return '火';
-    case DateTime.wednesday: return '水';
-    case DateTime.thursday: return '木';
-    case DateTime.friday: return '金'; 
-    case DateTime.saturday: return '土';
-    case DateTime.sunday: return '日';
-    default: return '';
+      case DateTime.monday:
+        return '月';
+      case DateTime.tuesday:
+        return '火';
+      case DateTime.wednesday:
+        return '水';
+      case DateTime.thursday:
+        return '木';
+      case DateTime.friday:
+        return '金';
+      case DateTime.saturday:
+        return '土';
+      case DateTime.sunday:
+        return '日';
+      default:
+        return '';
     }
   }
 }
