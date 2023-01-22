e8f922483dfd2724e7fe734125836bda1d734b28
seigibus <seigibus@icloud.com>
2023-01-22 16:14:33 +0900

    style: 不要なimport文を削除

--- lib/features/trips/controller/trip_controller.dart
+++ lib/features/trips/controller/trip_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
