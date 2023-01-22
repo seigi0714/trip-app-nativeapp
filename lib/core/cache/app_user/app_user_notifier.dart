5a9aca3fa5b68c39c0348059ec1303c2d7c09c33
shimizu-saffle <shimizu.saffle@gmail.com>
2023-01-22 12:21:01 +0900

    define: AppUserNotifier

---
+++ lib/core/cache/app_user/app_user_notifier.dart
@@ -0,0 +1,20 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:trip_app_nativeapp/core/cache/app_user/models/app_user.dart';
+import 'package:trip_app_nativeapp/core/http/api_client/api_client.dart';
+import 'package:trip_app_nativeapp/features/auth/controller/auth_controller.dart';
+
+part 'app_user_notifier.g.dart';
+
+@Riverpod(keepAlive: true)
+class AppUserNotifier extends _$AppUserNotifier {
+  @override
+  FutureOr<AppUser?> build() async {
+    final authUserAsync = ref.watch(firebaseAuthUserProvider);
+    if (authUserAsync.value != null) {
+      final res =
+          await ref.read(privateTripAppV1ClientProvider).get('/my/profile');
+      return AppUser.fromJson(res.data);
+    }
+    return null;
+  }
+}
---
+++ lib/core/cache/app_user/app_user_notifier.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
+
+part of 'app_user_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// ignore_for_file: avoid_private_typedef_functions, non_constant_identifier_names, subtype_of_sealed_class, invalid_use_of_internal_member, unused_element, constant_identifier_names, unnecessary_raw_strings, library_private_types_in_public_api
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+String $AppUserNotifierHash() => r'fb4d138f434c057a01c567f91f9564f19f952abd';
+
+/// See also [AppUserNotifier].
+final appUserNotifierProvider =
+    AsyncNotifierProvider<AppUserNotifier, AppUser?>(
+  AppUserNotifier.new,
+  name: r'appUserNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : $AppUserNotifierHash,
+);
+typedef AppUserNotifierRef = AsyncNotifierProviderRef<AppUser?>;
+
+abstract class _$AppUserNotifier extends AsyncNotifier<AppUser?> {
+  @override
+  FutureOr<AppUser?> build();
+}
