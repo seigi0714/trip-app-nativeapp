60613aed404a1994b4018bcab96fe3e1024d1c0e
seigibus <seigibus@icloud.com>
2023-01-22 14:47:10 +0900

    define: invitationエンティティを定義

---
+++ lib/features/trips/domain/entity/trip/trip_invitation.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:trip_app_nativeapp/features/trips/domain/entity/trip/value/trip_invitation_num.dart';
+
+part 'trip_invitation.freezed.dart';
+
+@Freezed(copyWith: false, fromJson: false, toJson: false)
+class TripInvitation with _$TripInvitation {
+  /// 新規作成時のfactory関数
+  factory TripInvitation.createNewTripInvitation({
+    required TripInvitationNum invitationNum,
+  }) = NewTripInvitation;
+
+  /// 作成済み旅エンティティのfactory関数
+  /// 現状一緒だけどcreateNewTripと内容変わるはずなので定義しておく
+  factory TripInvitation.createGeneratedTripInvitation({
+      required TripInvitationNum invitationNum,
+      required String invitationCode,
+  }) = GeneratedTripInvitation;
+}
