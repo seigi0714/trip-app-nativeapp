97189aebd8a9ce0e384277df99873ac464f798ed
shimizu-saffle <shimizu.saffle@gmail.com>
2023-01-25 22:22:14 +0900

    define: connectivityInterceptorProvider

---
+++ lib/core/http/api_client/dio/connectivity_interceptor.dart
@@ -0,0 +1,39 @@
+import 'package:dio/dio.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:trip_app_nativeapp/core/enum/dio_error_code.dart';
+import 'package:trip_app_nativeapp/core/http/network_connectivity.dart';
+
+part 'connectivity_interceptor.g.dart';
+
+@riverpod
+ConnectivityInterceptor connectivityInterceptor(
+  ConnectivityInterceptorRef ref,
+) {
+  return ConnectivityInterceptor(ref);
+}
+
+/// リクエスト時にネットワーク接続を確認するための Interceptor
+/// ApiClient で [DioErrorCode.networkNotConnected] をハンドリングする
+class ConnectivityInterceptor extends Interceptor {
+  ConnectivityInterceptor(this._ref);
+
+  final Ref _ref;
+
+  @override
+  Future<void> onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    final isNetworkConnected = _ref.read(networkConnectivityProvider);
+    if (!isNetworkConnected) {
+      return handler.reject(
+        DioError(
+          error: DioErrorCode.networkNotConnected,
+          requestOptions: options,
+        ),
+      );
+    }
+    return handler.next(options);
+  }
+}
