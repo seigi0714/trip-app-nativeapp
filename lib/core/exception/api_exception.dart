c6a4bfb1c4f098af45cccce0a2b1823931598d1d
shimizu-saffle <shimizu.saffle@gmail.com>
2023-01-25 21:08:18 +0900

    define: Subclass of ApiException

--- lib/core/exception/api_exception.dart
+++ lib/core/exception/api_exception.dart
@@ -1,17 +1,33 @@
+import 'dart:io';
+
 /// Trip App API との HTTP 通信時に使用する例外型。
 class ApiException implements Exception {
   const ApiException({
-    required this.statusCode,
+    this.statusCode,
     this.errorCode,
     this.description = 'サーバとの通信に失敗しました。',
   });
 
   /// HTTP ステータスコード
-  final int statusCode;
+  final int? statusCode;
 
-  /// Trip App API 独自のエラーコード
+  /// Trip App エラーコード
   final String? errorCode;
 
-  /// Trip App API が返すエラーについての説明
   final String? description;
+}
+
+class ApiTimeoutException extends ApiException {
+  const ApiTimeoutException({super.errorCode})
+      : super(
+          statusCode: HttpStatus.requestTimeout,
+          description: '通信環境をご確認のうえ、再度実行してください。',
+        );
+}
+
+class NetworkNotConnectedException extends ApiException {
+  const NetworkNotConnectedException({
+    super.statusCode,
+    super.errorCode,
+  }) : super(description: 'ネットワーク接続がありません。');
 }
