fc3f0ac0e0f82ef5def90da2e54013859bc077c3
shimizu-saffle <shimizu.saffle@gmail.com>
2023-01-25 21:38:14 +0900

    feat: ApiException とそのサブクラスのメンバを編集

--- lib/core/exception/api_exception.dart
+++ lib/core/exception/api_exception.dart
@@ -5,29 +5,33 @@ class ApiException implements Exception {
   const ApiException({
     this.statusCode,
     this.errorCode,
-    this.description = 'サーバとの通信に失敗しました。',
+    this.description,
+    this.message = 'サーバとの通信に失敗しました。',
   });
 
   /// HTTP ステータスコード
+  /// ネットワーク接続が無い場合も ApiException を投げるため、
+  /// null 許容型にしている。
   final int? statusCode;
 
   /// Trip App エラーコード
   final String? errorCode;
 
+  /// 開発者向けのエラー詳細
   final String? description;
+
+  /// ユーザー向けのエラーメッセージ
+  final String message;
 }
 
 class ApiTimeoutException extends ApiException {
-  const ApiTimeoutException({super.errorCode})
+  const ApiTimeoutException()
       : super(
           statusCode: HttpStatus.requestTimeout,
-          description: '通信環境をご確認のうえ、再度実行してください。',
+          message: '通信環境をご確認のうえ、再度実行してください。',
         );
 }
 
 class NetworkNotConnectedException extends ApiException {
-  const NetworkNotConnectedException({
-    super.statusCode,
-    super.errorCode,
-  }) : super(description: 'ネットワーク接続がありません。');
+  const NetworkNotConnectedException() : super(message: 'ネットワーク接続がありません。');
 }
