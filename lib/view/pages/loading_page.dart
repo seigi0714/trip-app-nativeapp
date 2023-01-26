d6dfd5764e770e0c1a3acfa5f929179dc9a2c4ce
shimizu-saffle <shimizu.saffle@gmail.com>
2023-01-26 09:19:46 +0900

    feat: LoadingPage はオーバーレイしてるように見せない

--- lib/view/pages/loading_page.dart
+++ lib/view/pages/loading_page.dart
@@ -9,8 +9,10 @@ class LoadingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: OverlayLoadingWidget(),
+    return const SizedBox.expand(
+      child: Center(
+        child: PrimarySpinkitCircle(),
+      ),
     );
   }
 }
