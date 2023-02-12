6e3814c3fcb6c42bd712c5364bd1cdc2daaec5b7
shimizu-saffle <shimizu.saffle@gmail.com>
2023-02-12 18:44:29 +0900

    define: FetchTripResponse

---
+++ lib/features/trips/data/models/fetch_trip_response.dart
@@ -0,0 +1,20 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:trip_app_nativeapp/core/json_converter/date_converter.dart';
+import 'package:trip_app_nativeapp/features/trips/data/models/fetch_trip_member_response.dart';
+
+part 'fetch_trip_response.freezed.dart';
+part 'fetch_trip_response.g.dart';
+
+@freezed
+class FetchTripResponse with _$FetchTripResponse {
+  const factory FetchTripResponse({
+    required int id,
+    required String name,
+    required List<FetchTripMemberResponse> members,
+    @DateConverter() required DateTime fromDate,
+    @DateConverter() required DateTime endDate,
+  }) = _FetchTripResponse;
+
+  factory FetchTripResponse.fromJson(Map<String, dynamic> json) =>
+      _$FetchTripResponseFromJson(json);
+}
