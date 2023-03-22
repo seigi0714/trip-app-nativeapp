8e8d25539d0f345ebba2cbf5571b4ab4eb7c4bc3
shimizu-saffle <shimizu.saffle@gmail.com>
2023-03-23 07:47:46 +0900

    refactor: rename to DebugPage and add assert and description

--- lib/view/pages/debug_page.dart
+++ lib/view/pages/debug_page.dart
@@ -1,14 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:trip_app_nativeapp/core/extensions/build_context.dart';
 import 'package:trip_app_nativeapp/features/auth/controller/auth_controller.dart';
 import 'package:trip_app_nativeapp/features/user/controller/app_user_controller.dart';
+import 'package:trip_app_nativeapp/view/pages/trips/trips_list_page.dart';
 
-class HomePage extends ConsumerWidget {
-  const HomePage({super.key});
+class DebugPage extends ConsumerWidget {
+  const DebugPage({super.key});
 
-  static const path = '/';
+  static const path = '/debug';
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -16,23 +17,26 @@ class HomePage extends ConsumerWidget {
       ref.read(appUserControllerProvider).value != null,
       'ログイン中はローカルにユーザー情報を保持します。',
     );
+    assert(
+      kDebugMode,
+      'リリースモードの場合、このページは表示されてはいけません。',
+    );
     return Scaffold(
       body: SafeArea(
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text(
-                'アプリのトップページ\n旅のしおり一覧をグリッド状に表示する',
-                style: context.textTheme.headlineSmall,
+              const Text(
+                'デバッグ用のページです。\n直接遷移したいページがある場合や、'
+                '\n実行したい処理がある場合は追加してください。',
               ),
-              const SizedBox(height: 16),
               ElevatedButton(
                 onPressed: () => ref.read(authControllerProvider).logOut(),
                 child: const Text('ログアウト'),
               ),
               ElevatedButton(
-                onPressed: () => context.go('/trips'),
+                onPressed: () => context.go(TripListPage.path),
                 child: const Text('旅一覧ページ'),
               ),
             ],
