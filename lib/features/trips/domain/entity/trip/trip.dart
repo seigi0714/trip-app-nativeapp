ddcc7fec7f651b5f8714c1f59c245bbd8cdf0e93
seigibus <seigibus@icloud.com>
2023-04-08 14:25:21 +0900

    feat: tripエンティティに作成前と作成後の型を用意

--- lib/features/trips/domain/entity/trip/trip.dart
+++ lib/features/trips/domain/entity/trip/trip.dart
@@ -7,44 +7,18 @@ part 'trip.freezed.dart';
 
 @Freezed(copyWith: false, fromJson: false, toJson: false)
 class Trip with _$Trip {
-  const factory Trip({
-    required TripTitle title,
-    required TripPeriod tripPeriod,
-
-    /// 新規作成時はメンバーがいないので null 許容
-    List<TripMember>? members,
-  }) = _Trip;
-
   /// 新規作成時のfactory関数
   factory Trip.createNewTrip({
-    required String title,
-    required DateTime fromDate,
-    required DateTime endDate,
-  }) {
-    return Trip(
-      title: TripTitle(value: title),
-      tripPeriod: TripPeriod(
-        fromDate: fromDate,
-        endDate: endDate,
-      ),
-    );
-  }
+    required TripTitle title,
+    required TripPeriod period,
+  }) = NewTrip;
 
   /// 作成済み旅エンティティのfactory関数
   /// 現状一緒だけどcreateNewTripと内容変わるはずなので定義しておく
   factory Trip.createExistingTrip({
-    required String title,
-    required DateTime fromDate,
-    required DateTime endDate,
+    required int id,
+    required TripTitle title,
+    required TripPeriod period,
     required List<TripMember> members,
-  }) {
-    return Trip(
-      title: TripTitle(value: title),
-      tripPeriod: TripPeriod(
-        fromDate: fromDate,
-        endDate: endDate,
-      ),
-      members: members,
-    );
-  }
+  }) = ExistingTrip;
 }
