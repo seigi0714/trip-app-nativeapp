b99d0f74e04e7dbd92bb8647f73f1cfa1bddcc91
seigibus <seigibus@icloud.com>
2023-05-16 22:17:13 +0900

    fix: 持ち物追加失敗時はスナックバー表示に変更

--- lib/features/trips/controller/trip_belonging_controller.dart
+++ lib/features/trips/controller/trip_belonging_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:trip_app_nativeapp/core/exception/exception_handler.dart';
 import 'package:trip_app_nativeapp/features/trips/domain/entity/trip/trip_belonging.dart';
 import 'package:trip_app_nativeapp/features/trips/domain/interactor/trip_interactor.dart';
 import 'package:trip_app_nativeapp/view/widgets/common/loading.dart';
@@ -20,17 +21,19 @@ class TripBelongingsController extends _$TripBelongingsController {
     required bool isShareAmongMember,
     VoidCallback? onFinished,
   }) async {
-    ref.read(overlayLoadingProvider.notifier).startLoading();
-    state = await AsyncValue.guard(() async {
+    try {
       final result = await ref.read(tripInteractorProvider).addTripBelonging(
             tripId: tripId,
             name: name,
             numOf: numOf,
             isShareAmongMember: isShareAmongMember,
           );
-      return [result, ...state.value ?? []];
-    });
-    ref.read(overlayLoadingProvider.notifier).endLoading();
-    onFinished?.call();
+      state = AsyncValue.data([result, ...state.value ?? []]);
+    } on Exception catch (e) {
+      ref.read(exceptionHandlerProvider).handleException(e);
+    } finally {
+      ref.read(overlayLoadingProvider.notifier).endLoading();
+      onFinished?.call();
+    }
   }
 }
