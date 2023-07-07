7805e33eccef34c8c446c18b920e7d99617224c2
shimizu-saffle <shimizu.saffle@gmail.com>
2023-07-07 22:22:47 +0900

    test: 呼び出し回数の確認もしておく

--- test/feature/trips/domain/trip_interactor_test.dart
+++ test/feature/trips/domain/trip_interactor_test.dart
@@ -150,6 +150,8 @@ void main() {
                 ),
             throwsA(isA<Exception>()),
           );
+
+          verify(mockTripRepo.updateTrip(trip: testExistingTrip)).called(1);
         },
       );
     },
